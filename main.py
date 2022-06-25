766ec88a5f2d5de4cdf53448e676a80aa152de1c
Tony Wijaya <tony18ti@mahasiswa.pcr.ac.id>
2022-06-25 22:26:19 +0700

    tambahkan kriteria crud

--- main.py
+++ main.py
@@ -103,3 +103,73 @@ def delete_data_pasien(pasien_id: int, db: Session = Depends(get_db)):
     db.commit()
     return f"Pasien with id : {pasien_id} deleted"
 
+
+@app.get("/kriteria_pasien")
+def get_data_kriteria_pasien(db: Session = Depends(get_db)):
+    return db.query(models.Kriteria).all()
+
+@app.post("/kriteria_pasien")
+def create_data_kriteria_pasien(pasien_kriteria: Kriteria, db:Session = Depends(get_db)):
+    
+    pasien_kriteria_model = models.Kriteria()
+
+
+    pasien_kriteria_model.gsr = pasien_kriteria.gsr
+    pasien_kriteria_model.hr = pasien_kriteria.hr
+    pasien_kriteria_model.bp = pasien_kriteria.bp
+    pasien_kriteria_model.suhu = pasien_kriteria.suhu
+    pasien_kriteria_model.respirasi = pasien_kriteria.respirasi
+    pasien_kriteria_model.id_pasien = pasien_kriteria.id_pasien
+
+    data = {'GSR_label': [int(pasien_kriteria.gsr)], 
+            'HR_label': [int(pasien_kriteria.hr)], 
+            'BP_label': [int(pasien_kriteria.bp)], 
+            'SUHU_label':[int(pasien_kriteria.suhu)], 
+            'RESPIRASI_label':[int(pasien_kriteria.respirasi)]}
+
+    # # Create DataFrame.
+    df = pd.DataFrame(data)
+    result = model.predict(df)
+    result = result.tolist()
+    result = ceklabelstres(result)
+
+    pasien_kriteria_model.tingkat_stress = result[0]
+
+    db.add(pasien_kriteria_model)
+    db.commit()
+    return pasien_kriteria_model
+
+@app.put("/kriteria_pasien/{kriteria_id}")
+def update_data_kriteria_pasien(kriteria_id: int, pasien_kriteria: Kriteria, db : Session = Depends(get_db)):
+    pasien_kriteria_model = db.query(models.Kriteria).filter(models.Kriteria.id_kriteria == kriteria_id).first()
+    if pasien_kriteria_model is None :
+        raise HTTPException(
+            status_code = 404,
+            detail = f"Kriteria ID {kriteria_id} : Does not exist"
+        )
+    pasien_kriteria_model.gsr = pasien_kriteria.gsr
+    pasien_kriteria_model.hr = pasien_kriteria.hr
+    pasien_kriteria_model.bp = pasien_kriteria.bp
+    pasien_kriteria_model.suhu = pasien_kriteria.suhu
+    pasien_kriteria_model.respirasi = pasien_kriteria.respirasi
+    pasien_kriteria_model.id_pasien = pasien_kriteria.id_pasien
+    pasien_kriteria_model.tanggal_cek = pasien_kriteria.tanggal_cek
+    db.add(pasien_kriteria_model)
+    db.commit()
+    return f"Kriteria Pasien with id : {kriteria_id} : Successfuly updated"
+
+@app.delete("/kriteria_pasien/{kriteria_id}")
+def delete_data_kriteria_pasien(kriteria_id: int, db: Session = Depends(get_db)):
+    pasien_kriteria_model = db.query(models.Kriteria).filter(models.Kriteria.id_kriteria == kriteria_id).first()
+    
+    if pasien_kriteria_model is None:
+        raise HTTPException(
+            status_code = 404,
+            detail = f"Kriteria Pasien with id : {kriteria_id} : Does not exist"
+        )
+    
+    db.query(models.Kriteria).filter(models.Kriteria.id_kriteria == kriteria_id).delete()
+    db.commit()
+    return f"Kriteria Pasien with id : {kriteria_id} deleted"
+
+
