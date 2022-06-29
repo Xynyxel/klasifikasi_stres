d12d30e448085976c52aa31ff3fc2309b2ec2199
Tony Wijaya <tony18ti@mahasiswa.pcr.ac.id>
2022-06-29 13:02:02 +0700

    Update main.py

--- main.py
+++ main.py
@@ -89,7 +89,7 @@ def update_data_pasien(pasien_id: int, pasien: Pasien, db : Session = Depends(ge
     db.commit()
     return f"Pasien with id : {pasien_id} : Successfuly updated"
 
-@app.put("/{pasien_id}")
+@app.put("/nama/{pasien_id}")
 def update_data_nama_pasien(pasien_id: int, pasien: Pasien, db : Session = Depends(get_db)):
     pasien_model = db.query(models.Pasien).filter(models.Pasien.id_pasien == pasien_id).first()
     if pasien_model is None :
