5c6377cd3efebd9b7b51f252fe6018cb00dd1278
Tony Wijaya <tony18ti@mahasiswa.pcr.ac.id>
2022-07-01 00:28:56 +0700

    kembalikan ke semula

--- main.py
+++ main.py
@@ -195,23 +195,4 @@ def onMessage(client, userdata, msg):
     
     # print(msg.topic + ": " + type(data))
 
-client = paho.Client()
-client.on_message = onMessage
-
-if client.connect("139.59.236.46", 1883, 60) != 0:
-    print("Could not connect to MQTT Broker!")
-    sys.exit(-1)
-
-# client.subscribe("building/nama")
-# client.subscribe("building/SPO2")
-# client.subscribe("building/SPO2_csv")
-client.subscribe("test/test")
-
-try:
-    print("Press CTRL+C to exit...")
-    client.loop_forever()
-except:
-    print("Disconnection from broker")
-
-client.disconnect()
 
