d267accb75e287cbb2264e201a5c41f1b364c702
Tony Wijaya <tony18ti@mahasiswa.pcr.ac.id>
2022-07-01 00:16:04 +0700

    coba update

--- mqtt_pub.py
+++ mqtt_pub.py
@@ -7,8 +7,7 @@ if client.connect("139.59.236.46", 1883, 60) != 0:
     print("Could not connect to MQTT Broker!")
     sys.exit(-1)
 
-test = [1,2,3,4]
-listTostr = ' '.join(str(elem) for elem in test)
-client.publish("test/test", listTostr, 0)
+
+client.publish("test/test", "test", 0)
 
 client.disconnect()
