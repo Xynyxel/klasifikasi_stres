bd5d5ee1b3cbf7a80cb69b76fe4056b1d8288f07
Tony Wijaya <tony18ti@mahasiswa.pcr.ac.id>
2022-06-30 22:55:25 +0700

    test readlines in file txt

--- test.py
+++ test.py
@@ -1,5 +1,8 @@
-quote = '成功を収める人とは人が投げてきたレンガでしっかりした基盤を築くことができる人のことである。'
+# quote = '成功を収める人とは人が投げてきたレンガでしっかりした基盤を築くことができる人のことである。'
 
-with open('test.txt', 'w', encoding='utf-8') as f:
-    f.write(quote)
-    f.readline()
+# with open('test.txt', 'w', encoding='utf-8') as f:
+#     f.write(quote)
+    
+
+f = open("test.txt", "r")
+f.readline()
