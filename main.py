2d5f3540e487ab2103e3a9294e9d4fb80ffc84b6
Tony Wijaya <tony18ti@mahasiswa.pcr.ac.id>
2022-07-01 00:38:16 +0700

    kembalikan ke semula

--- main.py
+++ main.py
@@ -11,40 +11,6 @@ import models
 from database import engine, SessionLocal
 from sqlalchemy.orm import Session
 
-import paho.mqtt.client as paho
-import sys
-
-def write_data(data):
-    with open('test.txt', 'w', encoding='utf-8') as f:
-        f.write(data)
-    print("berhasil")
-
-def onMessage(client, userdata, msg):
-    data = str(msg.payload.decode())
-    print(msg.topic + ": " + data)
-    write_data(data)
-    
-    # print(msg.topic + ": " + type(data))
-
-client = paho.Client()
-client.on_message = onMessage
-
-if client.connect("139.59.236.46", 1883, 60) != 0:
-    print("Could not connect to MQTT Broker!")
-    sys.exit(-1)
-
-# client.subscribe("building/nama")
-# client.subscribe("building/SPO2")
-# client.subscribe("building/SPO2_csv")
-client.subscribe("test/test")
-
-try:
-    print("Press CTRL+C to exit...")
-    client.loop_forever()
-except:
-    print("Disconnection from broker")
-    client.disconnect()
-
 
 app = FastAPI()
 
