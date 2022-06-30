e9a99790be2429596ddfc989d71045224b293249
Tony Wijaya <tony18ti@mahasiswa.pcr.ac.id>
2022-06-30 22:37:11 +0700

    Update mqtt_sub.py

--- mqtt_sub.py
+++ mqtt_sub.py
@@ -13,6 +13,7 @@ if client.connect("139.59.236.46", 1883, 60) != 0:
     sys.exit(-1)
 
 client.subscribe("building/nama")
+client.subscribe("building/SP02")
 
 try:
     print("Press CTRL+C to exit...")
