c32945e5842a9ddf2b400c78076ebde028c801b3
Tony Wijaya <tony18ti@mahasiswa.pcr.ac.id>
2022-07-04 23:14:41 +0700

    coba update data dataframe

--- main.py
+++ main.py
@@ -159,6 +159,7 @@ def create_data_kriteria_pasien(pasien_kriteria: Kriteria, db:Session = Depends(
             'SUHU_label':ceklabelSUHUintoModel(pasien_kriteria.suhu), 
             'RESPIRASI_label':ceklabelRESPIRASIintoModel(pasien_kriteria.respirasi)}
 
+    print(data)
     # # Create DataFrame.
     df = pd.DataFrame(data)
     result = model.predict(df)
