671598823adc0633804c7fcc34218c11ae1f27b7
Tony Wijaya <tony18ti@mahasiswa.pcr.ac.id>
2022-07-06 20:28:04 +0700

    Update main.py

--- main.py
+++ main.py
@@ -140,7 +140,7 @@ def get_data_kriteria_pasien_byidpasien(pasien_id: int, db: Session = Depends(ge
             status_code = 404,
             detail = f"Kriteria Pasien with id : {pasien_id} : Does not exist"
         )
-    return kriteria_pasien[0]
+    return kriteria_pasien[0]["tanggal_cek"]
 
 @app.post("/kriteria_pasien")
 def create_data_kriteria_pasien(pasien_kriteria: Kriteria, db:Session = Depends(get_db)):
