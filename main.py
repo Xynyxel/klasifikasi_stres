dd1c62d1b0e25578acfb7fcf9ee97dea4d393359
Tony Wijaya <tony18ti@mahasiswa.pcr.ac.id>
2022-07-20 16:23:52 +0700

    Update main.py

--- main.py
+++ main.py
@@ -77,7 +77,8 @@ def create_data_pasien(pasien: Pasien, db:Session = Depends(get_db)):
 
 @app.post("/login")
 def create_login_data_pasien(pasien_login : Pasien_Login, db:Session = Depends(get_db)):
-    pasien_model = db.query(models.Pasien).filter(models.Pasien.email == pasien_login.email and models.Pasien.password == pasien_login.password).first()
+    pasien_model = db.query(models.Pasien).filter(models.Pasien.email == pasien_login.email 
+    and models.Pasien.password == pasien_login.password).first()
     if pasien_model is None :
             raise HTTPException(
                 status_code = 404,
@@ -102,7 +103,8 @@ def update_data_pasien(pasien_id: int, pasien: Pasien, db : Session = Depends(ge
     return f"Pasien with id : {pasien_id} : Successfuly updated"
 
 @app.put("/password/{pasien_id}")
-def update_data_password_pasien(pasien_id: int, pasien_password: Pasien_Password, db : Session = Depends(get_db)):
+def update_data_password_pasien(pasien_id: int, pasien_password: Pasien_Password, db : 
+    Session = Depends(get_db)):
     pasien_model = db.query(models.Pasien).filter(models.Pasien.id_pasien == pasien_id).first()
     if pasien_model is None :
         raise HTTPException(
@@ -135,7 +137,9 @@ def get_data_kriteria_pasien(db: Session = Depends(get_db)):
 
 @app.get("/kriteria_pasien_last/{pasien_id}")
 def get_data_kriteria_pasien_byidpasien_last(pasien_id: int, db: Session = Depends(get_db)):
-    kriteria_pasien = db.query(models.Kriteria).filter(models.Kriteria.id_pasien == pasien_id).order_by(models.Kriteria.id_kriteria.desc()).first()
+    kriteria_pasien = db.query(models.Kriteria).filter(
+        models.Kriteria.id_pasien == pasien_id).order_by(
+            models.Kriteria.id_kriteria.desc()).first()
     if kriteria_pasien is None:
         raise HTTPException(
             status_code = 404,
@@ -145,7 +149,8 @@ def get_data_kriteria_pasien_byidpasien_last(pasien_id: int, db: Session = Depen
 
 @app.get("/kriteria_pasien/{pasien_id}")
 def get_data_kriteria_pasien_byidpasien(pasien_id: int, db: Session = Depends(get_db)):
-    kriteria_pasien = db.query(models.Kriteria).filter(models.Kriteria.id_pasien == pasien_id).all()
+    kriteria_pasien = db.query(models.Kriteria).filter(
+        models.Kriteria.id_pasien == pasien_id).all()
     for data in kriteria_pasien:
         # data.tanggal_cek = parser.parse(data.tanggal_cek)
         data.tanggal_cek  = data.tanggal_cek.strftime("%d/%m/%Y %H:%M:%S")
@@ -174,7 +179,6 @@ def create_data_kriteria_pasien(pasien_kriteria: Kriteria, db:Session = Depends(
             'BP_label': [ceklabelBPintoModel(pasien_kriteria_model.bp)], 
             'SUHU_label':[ceklabelSUHUintoModel(pasien_kriteria_model.suhu)], 
             'RESPIRASI_label':[ceklabelRESPIRASIintoModel(pasien_kriteria_model.respirasi)]}
-
 
     # Create DataFrame.
     df = pd.DataFrame(data)
